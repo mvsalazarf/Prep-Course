0f138f7ceed7ef60a6fb4d90a0723aeff2a78b11
Maria Salazar <mvsalazarflores@gmail.com>
2022-07-20 23:50:37 -0300

    homework II

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -197,6 +197,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  // return 'Hola '.concat(nombre, '!')
   return 'Hola' + ' ' + nombre + '!';
 }
 
